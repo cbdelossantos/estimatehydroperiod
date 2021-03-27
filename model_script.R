0df2c7a8a753e319995173282adcdef3ec68056e
Carmen B. de los Santos <76535516+cbdelossantos@users.noreply.github.com>
2021-03-27 08:46:23 +0000

    Update model_script.R

--- model_script.R
+++ model_script.R
@@ -5,39 +5,45 @@
 
 #### SETTINGS ####
 
-# libraries
-library(readxl)     # to read xlsx
-library(lubridate)  # for dates
-library(plyr)       # for ddply
-library(reshape2)   # shape data  
-library(ggplot2)    # for graphing
-library(grid)       # idem
-library(gridExtra)  # idem
-library(scales)     # idem
+# required packages
+packages <- c("readxl",       # to read xlsx
+              "lubridate",    # for handling dates
+              "plyr",         # for ddply function
+              "reshape2",     # to shape data
+              "ggplot2",      # for graphing
+              "grid",         # for graphing
+              "gridExtra",    # for graphing
+              "scales")       # for graphing
+for (i in seq_along(packages)) {
+  if(!do.call(require, list(package = packages[i]))) {
+    do.call(install.packages, list(pkgs = packages[i]))
+    do.call(require, list(package = packages[i]))
+  }
+}
 
 # clean
-rm(list=ls())
+rm(list = ls())
 
 # working directory
-setwd("~/OneDrive - Universidade do Algarve/02_projects/02_RIAVALUE/07_elevation_carbon_sequestration/estimatehydroperiod")
+setwd("~/OneDrive - Universidade do Algarve/Trabajo/STUDIES/wip/elevation/wordir/model/")
 
 # theme
 default <- theme(plot.background=element_blank()) +
-  theme(panel.background=element_rect(fill="white",colour="black")) +
+  theme(panel.background=element_rect(fill = "white", colour = "black")) +
   theme(strip.background=element_blank()) +
-  theme(strip.text=element_text(size=9,colour="black",angle=0)) +
+  theme(strip.text=element_text(size = 9,colour = "black", angle = 0)) +
   theme(panel.grid.major=element_blank()) +
   theme(panel.grid.minor=element_blank()) +
-  theme(axis.text.x=element_text(colour="black",size=10,angle=0)) +
-  theme(axis.text.y=element_text(colour="black",size=10,angle=0)) +
-  theme(axis.title.x=element_text(size=10,vjust=0.2)) +
-  theme(axis.title.y=element_text(size=10)) +
-  theme(plot.margin= unit(c(0.5,0.5,0.5,0.5),"lines")) +
+  theme(axis.text.x=element_text(colour = "black", size = 10, angle = 0)) +
+  theme(axis.text.y=element_text(colour = "black", size = 10, angle = 0)) +
+  theme(axis.title.x=element_text(size = 10, vjust = 0.2)) +
+  theme(axis.title.y=element_text(size = 10)) +
+  theme(plot.margin= unit(c(0.5, 0.5, 0.5, 0.5), "lines")) +
   theme(legend.background=element_blank()) +
   theme(legend.key=element_blank()) +
-  theme(legend.key.height=unit(0.8,"line")) + 
+  theme(legend.key.height=unit(0.8, "line")) + 
   theme(legend.text.align=0) +
-  theme(plot.title=element_text(size=16,face="bold"))
+  theme(plot.title=element_text(size = 16, face = "bold"))
 
 #### ------------------------------ MODEL -----------------------------------####
 #### INPUT - POINTS ####
@@ -49,22 +55,16 @@ data.poi <- read.csv("./inputs/data_points.csv")
 
 # load data (tide heights from official charts)
 data.hei <- read.csv("./inputs/data_heights.csv")
-data.hei$datetime <- as.POSIXct(
-  data.hei$datetime,
-  tz = "UTC")
+
+# date settings (must be UTC)
+data.hei$datetime <- as.POSIXct(data.hei$datetime, tz = "UTC")
 
 # check structure
 str(data.hei)
-data.hei$datetime[[1]] # this must be UTC - checked!
-
-# select data and rename columns
-data.hei <- data.hei[,c("datetime","height","tide")]
-data.hei$tide[data.hei$tide=="Preia-mar"] <- "high"
-data.hei$tide[data.hei$tide=="Baixa-mar"] <- "low"
 
 # calculate minutes from first date
 data.hei$timemin <- as.numeric(data.hei$datetime)/60
-data.hei$timemin <- data.hei$timemin-as.numeric(as.POSIXct("2017-03-01 00:00",tz="UTC"))/60
+data.hei$timemin <- data.hei$timemin-as.numeric(as.POSIXct("2017-03-01 00:00", tz = "UTC"))/60
 
 # add column day, month, time in hours
 data.hei$day   <- day(data.hei$datetime)
@@ -73,8 +73,8 @@ data.hei$timeh <- data.hei$timemin/60
 
 # check
 table(data.hei$tide,useNA="ifany")
-ggplot(data.hei,aes(x=timeh,y=height)) +
-  geom_point(size=0.8,colour="blue") + 
+ggplot(data.hei,aes(x = timeh, y = height)) +
+  geom_point(size = 0.8, colour = "blue") + 
   geom_line()
 
 #### MODEL-Part 1: CALCULATION INTERPOLATED TIDE HEIGHT ####
@@ -82,18 +82,19 @@ ggplot(data.hei,aes(x=timeh,y=height)) +
 # month of reference: March 2017.
 
 # create intervals
-data.int <- data.frame(datetime=seq(ISOdatetime(2017,2,28,0,0,0,tz="UTC"),
-                                    ISOdatetime(2017,4,1,0,0,0,tz="UTC"),
-                                    by=60*1)) # time interval in seconds
+data.int <- data.frame(datetime=seq(ISOdatetime(2017, 2, 28, 0, 0, 0, tz = "UTC"),
+                                    ISOdatetime(2017, 4, 1, 0, 0, 0, tz = "UTC"),
+                                    by = 60*1)) # time interval in seconds
 
 
 # calculate minutes from first date
 data.int$timemin <- as.numeric(data.int$datetime)/60
-data.int$timemin <- data.int$timemin-as.numeric(as.POSIXct("2017-03-01 00:00",tz="UTC"))/60
+data.int$timemin <- data.int$timemin-as.numeric(as.POSIXct("2017-03-01 00:00", tz = "UTC"))/60
 data.int <- merge(data.int,data.hei,all.x=T)
 
-# correction term for sea level rise since hydrographic zero was set
-# approximately 10 cm 
+# correction term for tide height from official chart (source: "Dado que o plano do Zero Hidrográfico (ZH) foi fixado em relação
+# a níveis médios adotados há várias décadas, existe presentemente uma diferença sistemática de cerca de +10 cm entre
+# as alturas de água observadas e as alturas de maré previstas. Para mais informações consultar www.hidrografico.pt).
 tide.corr <- 0.1
 
 # for each time event (t), i.e. row in data.int, identify previous/posterior tide event (time, height)
@@ -145,11 +146,11 @@ data.int$par_T <- data.int$post_time-data.int$prev_time   # time (min) between c
 data.int$par_t <- data.int$timemin - data.int$prev_time   # time (min) from previous event
 
 # calculate estimated height using analitical formula
-data.int$height <- with(data.int,ifelse(prev_event==post_event,prev_height,
-                                        (prev_height+post_height)/2+(prev_height-post_height)/2*cos((pi*par_t)/par_T)))
+data.int$height <- with(data.int,ifelse(prev_event == post_event,prev_height,
+                                        (prev_height + post_height)/2 + (prev_height-post_height)/2*cos((pi*par_t)/par_T)))
 
 # select columns
-data.int <- data.int[,c("datetime","timemin","height")]
+data.int <- data.int[,c("datetime", "timemin", "height")]
 
 # check time
 data.int$datetime[[1]] 
@@ -163,22 +164,22 @@ data.int$timeh <- data.int$timemin/60
 data.int <- data.int[data.int$month==3,]
 
 # select columns
-data.int <- data.int[c("datetime","day","timemin","timeh","height")]
+data.int <- data.int[c("datetime", "day", "timemin", "timeh", "height")]
 
 # check plot (only March)
-pdf("./outputs/plots_heights_chart_interpolated.pdf",onefile=TRUE,paper="a4")
-ggplot(data.int,aes(x=timeh,y=height)) +
-  geom_point(size=0.3) +
-  geom_point(data=data.hei[data.hei$month==3,],
-             aes(x=timeh,y=height),colour="red",shape=21) +
-  facet_wrap(.~day,scales="free_x")
+pdf("./outputs/plots_heights_chart_interpolated.pdf", onefile = TRUE, paper = "a4")
+ggplot(data.int,aes(x = timeh, y = height)) +
+  geom_point(size = 0.3) +
+  geom_point(data = data.hei[data.hei$month == 3,],
+             aes(x = timeh, y = height),colour = "red", shape = 21) +
+  facet_wrap(.~day,scales = "free_x")
 dev.off()
 
 # save
 write.csv(data.int,"./outputs/data_heights_interpolated.csv")
 
 #### MODEL-Part 2: CALCULATIONS DEPTHS and HYDROPERIODS ####
-# at each point p and time t, I want to estimate the water depth d
+# at each point p and time t, we want to estimate the water depth d
 # d(p,t) = h(t) - e(p)
 # where
     # d(p,t) is depth, in meters, at time t and point p
@@ -191,8 +192,8 @@ data.hei <- read.csv("./inputs/data_heights.csv")
 data.poi <- read.csv("./inputs/data_points.csv")
 
 # preparations for loop
-pdf("./outputs/plots_hydroperiod_days.pdf",onefile=TRUE,paper="a4")
-par(mfrow=c(3,3),pty="s",las=1,oma=c(1,1,1,1),mar=c(5,5,3,1))
+pdf("./outputs/plots_hydroperiod_days.pdf", onefile=TRUE, paper = "a4")
+par(mfrow = c(3, 3), pty = "s", las = 1, oma = c(1, 1, 1, 1), mar = c(5, 5, 3, 1))
 
 points    <- unique(data.poi$point)
 DATA.DEP  <- data.frame() # for depths over 1-min intervals over the month
@@ -212,8 +213,8 @@ for (i in 1:length(points)){
   data.dep           <- data.int                                 # h(t)
   data.dep$elevation <- table.mon$elevation                      # e(p)
   data.dep$depth     <- with(data.dep,                           # d(p,t) 
-                             ifelse(height>elevation,
-                                    height-elevation,
+                             ifelse(height > elevation,
+                                    height - elevation,
                                     0))         
 
   # add info point to data.dep
@@ -236,7 +237,7 @@ for (i in 1:length(points)){
   ## CALCULATE DAILY HYDROPERIOD
   for(j in 1:31){
     # select j day
-    subset <- data.dep[data.dep$day==j,]
+    subset <- data.dep[data.dep$day == j,]
     
     # create table
     table.day     <- data.poi[i,]
@@ -253,23 +254,25 @@ for (i in 1:length(points)){
     TABLE.DAY <- rbind(TABLE.DAY,table.day)
     
     # plot water depth over a day (BLACK)
-    plot(x=subset$timemin/60,y=subset$depth,type="l",
-         xlab="Time (hours of month)",
-         ylab="Water depth (m)",
-         ylim=c(-3,3),
-         pch=19,col="black")
+    plot(x = subset$timemin/60,
+         y = subset$depth,
+         type = "l",
+         xlab = "Time (hours of month)",
+         ylab = "Water depth (m)",
+         ylim = c(-3, 3),
+         pch = 19, col = "black")
     # add tide height from interpolations
-    lines(x=subset$timemin/60,y=subset$height,col="grey") 
+    lines(x = subset$timemin/60, y = subset$height, col = "grey") 
     # add low/high tide from charts (GREY CIRCLES)
     # points(x=data.hei$timemin/60,y=data.hei$height,col="grey")
     # add text point, day, hydroperiod
-    mtext(paste0(table.day$point," day ",j," - E = ",round(table.day$hydroperiod_hday,1)),
-          side=3,line=0,adj=0,cex=0.8)
+    mtext(paste0(table.day$point, " day ", j, " - E = ", round(table.day$hydroperiod_hday, 1)),
+          side = 3, line = 0, adj = 0, cex = 0.8)
     # add elevation line (GREEN LINE)
-    abline(a=table.day$elevation,b=0,col="green",lwd=1)
+    abline(a = table.day$elevation, b = 0, col = "green", lwd = 1)
     
   }
-  par(mfrow=c(3,3),pty="s",las=1,oma=c(1,1,1,1),mar=c(5,5,3,1))
+  par(mfrow = c(3, 3), pty = "s", las = 1, oma = c(1, 1, 1, 1), mar = c(5, 5, 3, 1))
   
   ## CALCULATE SAMPLING DAYS HYDROPERIOD
     # select days = c(28,29,30)
